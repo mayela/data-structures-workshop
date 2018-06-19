27e402c45e24a57ce0b72ee08f479e5dd9fb7b3b
Israel <rhizdo_nop@hotmail.com>
2018-06-19 13:05:56 -0500

    haciendo cambios arbol

--- js/tree.js
+++ js/tree.js
@@ -1,7 +1,8 @@
-function Node () {
+function Node (value) {
     constructor(value) {
         this.value = value; //Valor del nodo 
-        this.children = []; // lista de los nodos hijos
+        this.izquierdo;
+        this.derecho;
 
     }
 }
@@ -13,12 +14,42 @@ function Tree () {
        return this.root === null 
    }
 
-   add (value) {
+   add(value) {
        //Arbol no tiene elementos
-       if (this.isEmpty()){
+       if(this,isEmpty()){
            this.root = new Node(value)
            return
        }
    }
 
-   var aux = this.root 
+   var aux = this.root
+
+   while (aux) {
+     // vamos hacia la izquierda
+     if (value < aux.value) {
+       if (aux.left) {
+         aux = aux.left
+       } else {
+         aux.left = new Node(value)
+         return
+       }
+     } else { // vamos hacia la derecha
+       if (aux.right) {
+         aux = aux.right
+       } else {
+         aux.right = new Node(value)
+         return
+       }
+     }
+   }
+ }
+
+
+
+   /*
+var NodoRaiz = new Nodo(1); // Nodo raíz
+var NodoIzq = new Nodo(2); // Rama izquierda
+var NodoDer = new Nodo(2); //Rama derecha 
+
+NodoRaiz.izquierdo = NodoIzq;
+*/
